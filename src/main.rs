7cf575a7d1d582c8e820863f99115803a6d451c2
siya <del.siyamittal@gmail.com>
2023-07-05 23:00:05 +0530

    test code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod chunk;
+mod debug;
+
+use chunk::OpCode;
+use chunk::Chunk;
+
 fn main() {
     println!("Hello, bite!");
+    let mut chunk = Chunk::new();
+    chunk.write(OpCode::OP_RETURN as u8);
+    debug::disassemble_chunk(&chunk, "test_chunk");
+
 }
