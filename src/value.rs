da2f3d79f0f6e97090079ebb2a7f040c342f22bd
siya <del.siyamittal@gmail.com>
2023-07-12 15:16:06 +0530

    add bool and nil

--- src/value.rs
+++ src/value.rs
@@ -1,5 +1,52 @@
 pub enum Value {
-    Number(f64),
+    VAL_BOOL(bool),
+    VAL_NIL(())
+    VAL_NUMBER(f64),
+}
+
+impl From<Value> for bool {
+    fn from(value: Value) -> bool {
+        match value{
+            Value::VAL_BOOL(bool_val) => bool_val,
+            _ => println!("Error. Value is not boolean"),
+        }
+    }
+}
+
+impl From<Value> for f64 {
+    fn from(value: Value) -> f64 {
+        match value {
+            Value::VAL_NUMBER(num_val) => num_val,
+            _ => println!("Error. Value is not numeric"),
+        }
+    }
+}
+
+impl From<Value> for () {
+    fn from(value: Value) -> () {
+        match value {
+            Value::VAL_NIL(nil_val) => nil_val,
+            _ => println!("Error. Value is not nill"),
+        }
+    }
+}
+
+impl From<bool> for Value {
+    fn from(bool_val: bool) -> Value {
+        Value::VAL_BOOL(bool_val)
+    }
+}
+
+impl From<f64> for Value {
+    fn from(num_val: f64) -> Value {
+        Value::VAL_NUMBER(num_val)
+    }
+}
+
+impl From<()> for Value {
+    fn from(nil_val: ()) -> Value {
+        Value::VAL_NIL(nil_val)
+    }
 }
 
 impl Value {
